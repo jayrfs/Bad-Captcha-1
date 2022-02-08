79a6421ed7389af326fb2e8e15848a99dc7c305f
rfsjay <rfsjay@gmail.com>
2022-02-09 01:15:39 +0530

    Agitated Victorious Prior

--- main.js
+++ main.js
@@ -2,24 +2,35 @@ captcha =  document.getElementById("captcha");
 refreshButton = document.getElementById("refreshButton");
 submitButton = document.getElementById("submitButton");
 inputBox = document.getElementById("inputBox");
+
+inputBox.addEventListener("input", (event) => textchanged());
 inputBox.value = ""
 
+var passed = false;
 var a = Math.floor(Math.random() * 20) + 1;
 var b = Math.floor(Math.random() * 10) + 1;
 var op = ["*", "+", "/", "-"][Math.floor(Math.random()*4)];
-console.log(eval(a + op + b))
 
 captcha.innerText = `${a} ${op} ${b}`
 
 function verify(a, b, op) {
     if (eval(a + op + b) == inputBox.value){
-        console.log("pass")
-    }
-    else {
+        passed =  true;
+    } else {
         alert("WRONG CAPTCHA!")
         location.reload()
     }
 }
 
+function textchanged() {
+    verify(a, b, op)
+}
+
 refreshButton.onclick =  function() {location.reload()}
-submitButton.onclick =  function() {verify(a, b, op)}
+submitButton.onclick =  function() {
+    verify(a, b, op)
+    if (passed == true) {
+        alert("Congratulations, you have passed this captcha! :)")
+        window.location.href = "https://www.youtube.com/watch?v=nUKgeAKTaXM";
+    }
+}
